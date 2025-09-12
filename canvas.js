aa6ab13954a6f0857cd0a09fcb5c1532b05de834
Ice424 <e4elliotd@gmail.com>
2025-09-12 19:28:53 +0100

    fancy Box

--- canvas.js
+++ canvas.js
@@ -1,23 +1,32 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const NUM_POINTS = 80;
+const BORDER = 20
+
+var width = 0
+var height = 0
+var points
+
+
 // Resize canvas to fill the window
 function resize() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  width = canvas.width;
+  height = canvas.height;
+  points = Array.from({ length: NUM_POINTS }, () => [
+  Math.random() * width,
+  Math.random() * height,
+]);
 }
 resize();
 window.addEventListener("resize", resize);
 
-const WIDTH = canvas.width;
-const HEIGHT = canvas.height;
-const NUM_POINTS = 80;
+
+
 
 // Generate random points
-let points = Array.from({ length: NUM_POINTS }, () => [
-  Math.random() * WIDTH,
-  Math.random() * HEIGHT,
-]);
 
 // Give each point a velocity
 let velocities = points.map(() => [
@@ -25,21 +34,25 @@ let velocities = points.map(() => [
   (Math.random() - 0.5) * 4, // vy
 ]);
 
+
+
+
+
 function update() {
   // Move points and bounce off edges
   for (let i = 0; i < points.length; i++) {
     points[i][0] += velocities[i][0];
     points[i][1] += velocities[i][1];
 
-    if (points[i][0] < 0 || points[i][0] > WIDTH) velocities[i][0] *= -1;
-    if (points[i][1] < 0 || points[i][1] > HEIGHT) velocities[i][1] *= -1;
+    if (points[i][0] < 0 - BORDER|| points[i][0] > width + BORDER) velocities[i][0] *= -1;
+    if (points[i][1] < 0 - BORDER|| points[i][1] > height + BORDER) velocities[i][1] *= -1;
   }
 }
 
 function draw() {
   ctx.strokeStyle = "black";
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  ctx.fillRect(0,0,WIDTH,HEIGHT);
+  ctx.clearRect(0, 0, width, height);
+  ctx.fillRect(0,0,width,height);
   ctx.strokeStyle = "white";
   ctx.lineWidth = 1;
 
