d9b6a4653126794ecf265b913c8fac610ee4270e
ice424 <e4elliotd@gmail.com>
2025-10-07 21:56:15 +0100

    Scrolling

--- canvas.js
+++ canvas.js
@@ -13,16 +13,16 @@ var grad
 // Resize canvas to fill the window
 function resize() {
   canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  canvas.height = window.innerHeight + BORDER;
   width = canvas.width;
   height = canvas.height;
   points = Array.from({ length: NUM_POINTS }, () => [
-  Math.random() * (width+BORDER),
-  Math.random() * (height+BORDER),
+    Math.random() * (width + BORDER),
+    Math.random() * (height),
 
-  
-]);
-  grad=ctx.createLinearGradient(0,height-(height*0.3), 0,height);
+
+  ]);
+  grad = ctx.createLinearGradient(0, height - (height * 0.3), 0, height);
   grad.addColorStop(0, "black");
   grad.addColorStop(1, getComputedStyle(document.body).getPropertyValue('--background'));
 
@@ -51,18 +51,25 @@ function update() {
     points[i][0] += velocities[i][0];
     points[i][1] += velocities[i][1];
 
-    if (points[i][0] < 0 - BORDER|| points[i][0] > width + BORDER) velocities[i][0] *= -1;
-    if (points[i][1] < 0 - BORDER|| points[i][1] > height + BORDER) velocities[i][1] *= -1;
+    if (points[i][0] < 0 - BORDER || points[i][0] > width + BORDER) velocities[i][0] *= -1;
+    if (points[i][1] < 0 - BORDER || points[i][1] > height) velocities[i][1] *= -1;
   }
 }
-let x = Math.random() * (width+BORDER)
-let y = Math.random() * (height+BORDER)
+let x = Math.random() * (width + BORDER)
+let y = Math.random() * (height)
 document.addEventListener("mousemove", updateCursorPos);
+document.addEventListener("scroll", updateCursorPos);
 
 function updateCursorPos(event) {
+  // Get the cursor relative to the canvas
   x = event.pageX;
-  y = event.pageY;
+  y = event.pageY + document.body.scrollTop;
+
+  // Clamp to canvas bounds
+  x = (x + width) % width;
+  y = (y + height) % height;
 }
+
 
 
 
@@ -70,20 +77,20 @@ function draw() {
   ctx.fillStyle = "black"
   ctx.strokeStyle = "black";
   ctx.clearRect(0, 0, width, height);
-  ctx.fillRect(0,0,width,height);
+  ctx.fillRect(0, 0, width, height);
   ctx.strokeStyle = "white";
   ctx.lineWidth = 1;
 
 
   ctx.fillStyle = grad;
-  ctx.fillRect(0,height - (height*0.3), width, height*0.3 );
+  ctx.fillRect(0, height - (height * 0.3), width, height * 0.3);
   points[0] = [x, y]
-  
+
   const delaunay = d3.Delaunay.from(points);
   const triangles = delaunay.triangles;
 
 
-  
+
   // Draw triangle edges
   for (let i = 0; i < triangles.length; i += 3) {
     let a = points[triangles[i]];
@@ -101,12 +108,12 @@ function draw() {
   ctx.fillStyle = "white"
   // Draw points as small white dots
   for (let [x, y] of points) {
-    
+
     ctx.beginPath();
     ctx.arc(x, y, 5, 0, Math.PI * 2);
     ctx.fill();
   }
-  
+
 
 }
 
