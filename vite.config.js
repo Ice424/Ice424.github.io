49ad866b9f996b2fce0d7dd04e10e06004374e40
Ice424 <e4elliotd@gmail.com>
2025-10-14 12:37:21 +0100

    it now should work

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+// ✅ since your repo name *is* your GitHub Pages URL root (ice424.github.io),
+// you can leave base as '/'
+export default defineConfig({
+  base: '/',
+})
