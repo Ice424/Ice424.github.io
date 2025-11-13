b88eb3105da1c724840f3b382dd9dcc5b34be571
ice424 <e4elliotd@gmail.com>
2025-11-13 20:19:24 +0000

    FINAL CHANGES

---
+++ src/werewolf-scene.js
@@ -0,0 +1,92 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+
+function main() {
+  const canvas = document.querySelector('#werewolf-scene');
+  const renderer = new THREE.WebGLRenderer({ antialias: false, canvas, alpha: true });
+
+  const camera = new THREE.PerspectiveCamera(90, canvas.clientWidth / canvas.clientHeight, 0.1, 100);
+  camera.position.set(0, 1, -2);
+  camera.lookAt(0, 1, 0);
+
+  function resize_render() {
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+    camera.aspect = (canvas.clientWidth / canvas.clientHeight)
+    camera.updateProjectionMatrix()
+  }
+
+  resize_render();
+  window.addEventListener("resize", resize_render);
+
+  
+
+  const base_rotation = camera.rotation.clone();
+  const scene = new THREE.Scene();
+
+  const light = new THREE.AmbientLight(0xffffff, 3);
+  scene.add(light);
+
+  const gltfLoader = new GLTFLoader();
+
+
+  let mixer; // will control animations
+  const clock = new THREE.Clock(); // keeps consistent animation timing
+
+  gltfLoader.load("/model/werewolf.gltf", (gltf) => {
+    const root = gltf.scene;
+    scene.add(root);
+
+    if (gltf.animations && gltf.animations.length > 0) {
+      mixer = new THREE.AnimationMixer(root);
+
+      // Play first animation (you can loop through gltf.animations to see more)
+      const action = mixer.clipAction(gltf.animations[0]);
+      action.play();
+      action.setLoop(THREE.LoopRepeat); // ensures looping
+    }
+  });
+
+  let panx = 0;
+  let pany = 0;
+
+  document.addEventListener('mousemove', updatePanCursorPos);
+  document.addEventListener('scroll', updatePanCursorPos);
+
+  function updatePanCursorPos(event) {
+    const rect = canvas.getBoundingClientRect();
+    const relX = event.clientX - rect.left;
+    const relY = event.clientY - rect.top;
+
+    const normX = ((relX / rect.width) - 0.5) * 2;
+    const normY = ((relY / rect.height) - 0.5) * 2;
+
+    panx = normX * 0.01;
+    pany = normY * 0.03;
+  }
+
+  function render() {
+    const delta = clock.getDelta();
+
+    if (mixer) mixer.update(delta); // advance animation
+
+    const new_rotation = new THREE.Euler(
+      base_rotation.x + pany,
+      base_rotation.y + panx,
+      base_rotation.z,
+      'XYZ'
+    );
+
+    camera.rotation.x = THREE.MathUtils.lerp(camera.rotation.x, new_rotation.x, 0.05);
+    camera.rotation.y = THREE.MathUtils.lerp(camera.rotation.y, new_rotation.y, 0.05);
+    camera.rotation.z = THREE.MathUtils.lerp(camera.rotation.z, new_rotation.z, 0.05);
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+}
+
+main();
